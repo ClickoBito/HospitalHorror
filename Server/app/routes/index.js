85455633fc020d436e69dfb8ab09474f30cf1048
Patrick Franz <patfra71@gmail.com>
2018-04-05 10:23:32 +0200

    Made improvements to login-process

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -10,17 +10,16 @@ const AuthCtrl = require('../controllers/auth.js');
 router.post('/login', AuthCtrl.login);
 router.post('/logout', AuthCtrl.logout);
 
+// TODO: put function in a controller
 router.get('/home/:id', function(req, res) {
 	res.sendFile('./public/views/home.html', { root: __dirname + "./../.." });
 });
 
 // frontend routes =========================================================
-// route to handle all Angular requests
+// route to handle all other requests
 router.get('*', function(req, res) {
 	res.sendFile('./public/views/index.html', { root: __dirname + "./../.." });
 });
-
-
 
 
 module.exports = router;
