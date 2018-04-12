43fdd2957476c78c3fd30d77352f925397f02e82
Teklie <teklie@student.chalmers.se>
2018-04-12 17:11:10 +0200

    Patientinfo route and controller has been added

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -4,12 +4,21 @@ const router = express.Router();
 
 // controller
 const AuthCtrl = require('../controllers/auth.js');
+const PatientInfoCtrl = require('../controllers/patientinfo.js');
 
 
 // RESTful API
+
+// Admin
 router.post('/register', AuthCtrl.register);
 router.post('/login', AuthCtrl.login);
 router.post('/logout', AuthCtrl.logout);
+
+// PatientInfo
+router.post('/patientinfo', PatientInfoCtrl.create);
+router.put('/patientinfo', PatientInfoCtrl.edit);
+router.delete('/patientinfo', PatientInfoCtrl.delete);
+
 
 // TODO: put function in a controller
 router.get('/home/:id', function(req, res) {
