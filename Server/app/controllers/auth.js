8e7afecef048241341e050977d826f9b38c9f40d
Seblerr <sibbe-95@hotmail.com>
2018-04-12 15:42:38 +0200

    Finished admin registration function

--- Server/app/controllers/auth.js
+++ Server/app/controllers/auth.js
@@ -41,9 +41,44 @@ module.exports.register = function (req, res, next) {
 		where: {username: req.body.username},
 		defaults: {password: req.body.password, userType: req.body.usertype}
 	}).spread((user, created) => {
+		//Check if user was created or if it already exists
 		if (created) {
+			//Check user type and create corresponding model
+			if (req.body.usertype === 'Doctor') {
+				model.Doctor.create({
+					firstname: req.body.firstname, lastname: req.body.lastname,
+					dateofbirth: req.body.dateofbirth, phone: req.body.phone,
+					email: req.body.email, UserId: user.id
+				}).then(doctor => {
+					// let info = doctor.get({plain:true})
+					console.log('Doctor created');
+				})
+			}
+
+			else if (req.body.usertype === 'Nurse') {
+				model.Nurse.create({
+						firstname: req.body.firstname, lastname: req.body.lastname,
+						dateofbirth: req.body.dateofbirth, phone: req.body.phone,
+						email: req.body.email, UserId: user.id
+					}).then(nurse => {
+						// let info = nurse.get({plain:true})
+						console.log('Nurse created');
+					})
+				}
+
+			else {
+				model.Secretary.create({
+					firstname: req.body.firstname, lastname: req.body.lastname,
+					dateofbirth: req.body.dateofbirth, phone: req.body.phone,
+					email: req.body.email, UserId: user.id
+				}).then(secretary => {
+					// let info = secretary.get({plain:true})
+					console.log('Secretary created');
+				})
+			}
 			console.log('User successfully created');
 			res.redirect('/');
+
 		}
 		else {
 			console.log('User with this username already found');
