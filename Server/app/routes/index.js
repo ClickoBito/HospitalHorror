754a4f5200ee48399d0a5fda8efd49446568e5d4
Carl Jansson <carl.jansson.contractor@ensighten.com>
2018-04-12 18:10:52 +0200

    revert routes/index.js changes

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const server = express();
 
 // controller
 const AuthCtrl = require('../controllers/auth.js');
@@ -12,33 +11,21 @@ router.post('/register', AuthCtrl.register);
 router.post('/login', AuthCtrl.login);
 router.post('/logout', AuthCtrl.logout);
 
-
 // TODO: put function in a controller
-const getHomeIDController = (req, res) => {
+router.get('/home/:id', function(req, res) {
 	res.sendFile('./views/home.html', { root: __dirname + "./../.." });
-}
+});
 
-router.get('/home/:id', getHomeIDController);
-
-// TODO: put function in a controller
-const getAdminController = (req, res) => {
+router.get('/admin', function (req, res) {
 	console.log('Admin logged in');
 	res.sendFile('./views/admin.html', { root: __dirname + "./../.." });
-}
-
-router.get('/admin', getAdminController);
+});
 
 // frontend routes =========================================================
-
-// TODO: put function in a controller
-const indexController = (req, res) => {
-	console.log('res.sendFile... index.html');
-	res.sendFile('./views/index.html', { root: __dirname + "./../.." });
-	// res.status(200).send({hey: 'hey'});
-}
-
 // route to handle all other requests
-router.get('*', indexController);
+router.get('*', function(req, res) {
+	res.sendFile('./views/index.html', { root: __dirname + "./../.." });
+});
 
 
 module.exports = router;
