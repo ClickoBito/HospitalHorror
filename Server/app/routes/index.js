aac41541311e10288813260dd31ef626f22a5240
Teklie <teklie@student.chalmers.se>
2018-04-12 21:38:41 +0200

    Route and controller for patientallergy

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // controller
 const AuthCtrl = require('../controllers/auth.js');
 const PatientInfoCtrl = require('../controllers/patientinfo.js');
-
+const PatientAllergyCtrl = require('../controllers/patientallergy.js');
 
 // RESTful API
 
@@ -19,6 +19,10 @@ router.post('/patientinfo', PatientInfoCtrl.create);
 router.put('/patientinfo/:id', PatientInfoCtrl.edit);
 router.delete('/patientinfo/:id', PatientInfoCtrl.delete);
 
+// PatientAllergy
+router.post('/patientallergy', PatientAllergyCtrl.create);
+router.put('/patientallergy/:id', PatientAllergyCtrl.edit);
+router.delete('/patientallergy/:id', PatientAllergyCtrl.delete);
 
 // TODO: put function in a controller
 router.get('/home/:id', function(req, res) {
