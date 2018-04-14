a74f28955918e513a07d8494f132126e73f2aea5
Patrick Franz <patfra71@gmail.com>
2018-04-14 03:07:44 +0200

    Restored login-page as default

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -37,7 +37,7 @@ router.get('/admin', function (req, res) {
 // frontend routes =========================================================
 // route to handle all other requests
 router.get('*', function(req, res) {
-	res.sendFile('./views/home.html', { root: __dirname + "./../.." });
+	res.sendFile('./views/index.html', { root: __dirname + "./../.." });
 });
 
 
