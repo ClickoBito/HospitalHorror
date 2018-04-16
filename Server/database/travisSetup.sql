7f1e8110609fafbb529a8b53e0e46e4f3d43ee63
Carl Jansson <carl.jansson.contractor@ensighten.com>
2018-04-16 02:37:06 +0200

    Another attempt to make Travis and Maria happy

--- Server/database/travisSetup.sql
+++ Server/database/travisSetup.sql
@@ -1,9 +1,12 @@
+CREATE DATABASE IF NOT EXISTS 'hospitalHorror';
+GRANT ALL ON hospitalHorror.* TO 'root'@'localhost';
+
 # Create database user
-DROP USER 'admin'@'localhost';
-flush privileges;
-CREATE USER 'admin'@'localhost';
+# DROP USER 'admin'@'localhost';
+# flush privileges;
+# CREATE USER 'admin'@'localhost';
 # GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'admin'@'localhost';
 
 # Create DB
-CREATE DATABASE IF NOT EXISTS `hospitalHorror`; # DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `hospitalHorror`;
+# CREATE DATABASE IF NOT EXISTS 'hospitalHorror'; # DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+# USE 'hospitalHorror';
