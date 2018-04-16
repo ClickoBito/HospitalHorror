66228ba4215f59f8a18174668eaf0efeec2387d4
Carl Jansson <carl.jansson.contractor@ensighten.com>
2018-04-16 02:59:31 +0200

    Re-enabled slack notifications for Travis

--- Server/database/travisSetup.sql
+++ Server/database/travisSetup.sql
@@ -1,12 +1,2 @@
 CREATE DATABASE hospitalHorror;
 GRANT ALL ON hospitalHorror.* TO 'root'@'localhost';
-
-# Create database user
-# DROP USER 'admin'@'localhost';
-# flush privileges;
-# CREATE USER 'admin'@'localhost';
-# GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'admin'@'localhost';
-
-# Create DB
-# CREATE DATABASE IF NOT EXISTS 'hospitalHorror'; # DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-# USE 'hospitalHorror';
