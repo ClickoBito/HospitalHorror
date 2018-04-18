f1061bdff1d1e2cc58dae294c2fe85c182ee9ae0
Amaarillo <Amar_kulaglic@hotmail.com>
2018-04-18 16:36:30 +0200

    Changed names in treatment.js

--- Server/app/controllers/treatment.js
+++ Server/app/controllers/treatment.js
@@ -1,8 +1,12 @@
 const model = require('../models/');
 
+module.exports.createTreatment = function(req, res, next) { // create is a function in a sequelize
 	console.log('Trying to create a Treatment');
 	console.log(req.body);
 	model.Treatment.create(req.body)//creating a treatment
+	.then(response => {
+		console.log('Trying to create a Treatment');
+		console.log(response.get({plain:true}));
 		// TODO: redirect to some page
 		res.redirect('/');
 	}, err => {
@@ -13,14 +17,19 @@ const model = require('../models/');
 };
 
 module.exports.getTreatmentById = function(req, res, next) {
+	model.Treatment.findById(req.body.id).then(response => {
+        console.log('The treatment fetched is: ', response.get({plain:true}));
         // TODO: need to update which view that needs the treatment
         //res.render('treatment', {
+        //   treatment: response});
     });
 };
 
+module.exports.editTreatmentById = function(req, res, next) {
 	console.log('Trying to update a Treatment');
 	console.log(req.body);
 	model.Treatment.update(req.body,{where: {id: req.body.id}})
+	.then(response => {
 		console.log('Updated Treatment');
 		// TODO: redirect to some page
 		res.redirect('/');
@@ -31,8 +40,10 @@ module.exports.getTreatmentById = function(req, res, next) {
 	});
 };
 
+module.exports.deleteTreatmentById = function(req, res, next) {
 	console.log('Trying to delete a Treatment');
 	model.Treatment.destroy({where: {id: req.body.id}})
+	.then(response => {
 		console.log('Deleted Treatment');
 		// TODO: redirect to some page
 		res.redirect('/');
