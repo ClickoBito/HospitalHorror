6600b84f32f99d75ec7f039170566fc10112d418
Amaarillo <Amar_kulaglic@hotmail.com>
2018-04-21 22:15:58 +0200

    Mocha tests for the treatment
    Implemented tests for creating, editing and deleting a treatment

--- Server/app/controllers/treatment.js
+++ Server/app/controllers/treatment.js
@@ -9,11 +9,37 @@ module.exports.createTreatment = function(req, res, next) { // create is a funct
 		console.log(err);
 		// TODO: redirect to error page e.g.
 		res.redirect('/error/');
+	});
+};
 
+module.exports.editTreatment = function(req, res, next) {
+	model.Treatment.update(req.body,{where: {id: req.params.id}})
+	.then(response => {
+		// TODO: redirect to some page
+		res.redirect('/');
+	}, err => {
+		console.log(err);
+		// TODO: redirect to error page e.g.
+		res.redirect('/error/');
+	});
+};
+
+module.exports.deleteTreatment = function(req, res, next) {
+	model.Treatment.destroy({where: {id: req.params.id}})
+	.then(response => {
+		// TODO: redirect to some page
+		res.redirect('/');
+	}, err => {
+		console.log(err);
+		// TODO: redirect to error page e.g.
+		res.redirect('/error/');
 	});
 };
 
 /*
+// TODO: Update if this is needed or not
+// Unsure if this will be used later
+
 module.exports.getTreatment = function(req, res, next) {
 	model.Treatment.findById(req.params.id).then(response => {
 		//console.log(response);
@@ -27,27 +53,3 @@ module.exports.getTreatment = function(req, res, next) {
     });
 };
 */
-
-module.exports.editTreatment = function(req, res, next) {
-	model.Treatment.update(req.body,{where: {id: req.params.id}})
-	.then(response => {
-		// TODO: redirect to some page
-		res.redirect('/');
-	}, err => {
-		console.log(err);
-		// TODO: redirect to error page e.g.
-		res.redirect('/');
-	});
-};
-
-module.exports.deleteTreatment = function(req, res, next) {
-	model.Treatment.destroy({where: {id: req.params.id}})
-	.then(response => {
-		// TODO: redirect to some page
-		res.redirect('/');
-	}, err => {
-		console.log(err);
-		// TODO: redirect to error page e.g.
-		res.redirect('/');
-	});
-};
--- Server/test/app/routes/treatment.test.js
+++ Server/test/app/routes/treatment.test.js
@@ -17,10 +17,19 @@ function sleep(ms) {
     }
 }
 
-// Example test creating treatment
-describe('Test of creating a treatment', () => {
-    it('Test of creating a treatment by checking if it is created', done => {
-        // Create a treatment in treatment table
+describe('Test of creating treatments', function() {
+    beforeEach(function (done) {
+        // Create 5 treatments to store them to the database
+        for(var i=0; i<4; i++){
+            supertest(app)
+            .post('/treatment')
+            .send({description: 'Treatment created', TreatmentTypeId: '2'})
+            .expect(302)
+            .end((err, res) => {
+                should.not.exist(err);
+                res.headers.location.substring(0,6).should.not.equal('/error/');
+            });
+        }
         supertest(app)
         .post('/treatment')
         .send({description: 'Treatment created', TreatmentTypeId: '2'})
@@ -28,61 +37,153 @@ describe('Test of creating a treatment', () => {
         .end((err, res) => {
             should.not.exist(err);
             res.headers.location.substring(0,6).should.not.equal('/error/');
-            // Get latest created entry in treatment table
-            model.Treatment.findAll({
-                limit: 1,
+            //Sleep is necesarry to store all new treatments in the database before the tests begin
+            sleep(500);
+            done();
+        });
+    });
+    afterEach(function (done) {
+        // Remove the 5 treatments that have been used for the tests
+        model.Treatment.findAll({
+            limit: 5,
+            where: {
+            },
+            order: [['createdAt', 'DESC'], ['id', 'DESC']]
+        }).then(function(treatments) {
+            model.Treatment.destroy({
                 where: {
-                },
-                order: [['createdAt', 'DESC']]
-            }).then(function(treatments) {
-                treatments[0].description.should.equal('Treatment created');
-                treatments[0].TreatmentTypeId.should.equal(2);
-                // Delete latest created entry in treatment table
-                model.Treatment.destroy({
-                    where: {
-                        id: treatments[0].id
-                    }
-                });
+                    [Op.or]: [{id: treatments[0].id},{id: treatments[1].id}, 
+                    {id: treatments[2].id}, {id: treatments[3].id}, {id: treatments[4].id}]
+                }
+            }).then(function(){
                 done();
             });
         });
     });
-    it('Test of creating 5 treatment by checking if their ids\' are correct', done => {
-        var count = 0;
-        for(var i=0; i<5; i++){
-            sleep(200);
-            // Create 5 treatments in treatment table
+    it('Checks if the 5 treatments has the correct content', function (done) {
+        // Get latest created entries in treatment table
+        model.Treatment.findAll({
+        limit: 5,
+        where: {
+        },
+        order: [['createdAt', 'DESC'], ['id', 'DESC']]
+        }).then(function(treatments) {
+            for(var i=0; i<5; i++){
+                treatments[i].description.should.equal('Treatment created');
+                treatments[i].TreatmentTypeId.should.equal(2);
+            }
+            done();
+        });
+    });
+    it('Check if the treatments have the correct ids', function(done) {
+        // Get the 5 latest created entries in treatment table
+        model.Treatment.findAll({
+            limit: 5,
+            where: {
+            },
+            order: [['createdAt', 'DESC'], ['id', 'DESC']]
+        }).then(function(treatments) {
+            for(var j=0; j<4; j++){
+                // Check if all ids' are different and if every id is increased by 1 after creating a treatment
+                (treatments[j].id-treatments[j+1].id).should.equal(1);
+            }
+            done();
+        });
+    });
+});
+
+describe('Test of editing a treatment', function() {
+    beforeEach(function (done) {
+        var data = {description: 'Treatment created', TreatmentTypeId: '2'};
+        model.Treatment.create(data)//creating a treatment
+	    .then(response => {
+            done();
+	    });
+    });
+    afterEach(function (done) {
+        // Remove the treatment that has been used for tests
+        model.Treatment.findAll({
+            limit: 1,
+            where: {
+            },
+            order: [['createdAt', 'DESC']]
+        }).then(function(treatments) {
+            model.Treatment.destroy({
+                where: {
+                    id: treatments[0].id
+                }
+            }).then(function(){
+                done();
+            });
+        });
+    });
+    it('Test of editing a treatment to check that the data has been changed', function (done) {
+        // Fetch latest created entry from treatment table
+        model.Treatment.findAll({
+            limit: 1,
+            where: {
+            },
+            order: [['createdAt', 'DESC']]
+        }).then(function(treatments) {
+            //Send a request to database for editing a treatment
             supertest(app)
-            .post('/treatment')
+            .put('/treatment/' + treatments[0].id)
+            .send({description: 'Treatment edited', TreatmentTypeId: '1'})
             .expect(302)
             .end((err, res) => {
                 should.not.exist(err);
                 res.headers.location.substring(0,6).should.not.equal('/error/');
-                    if(count => 5) {
-                        // Get the 5 latest created entries in treatment table
-                        model.Treatment.findAll({
-                            limit: 5,
-                            where: {
-                            },
-                            order: [['createdAt', 'DESC'], ['id', 'DESC']]
-                        }).then(function(treatments) {
-                            for(var j=0; j<4; j++){
-                                // Check if all ids' are different and if every id is increasing by 1 after creating a treatment
-                                (treatments[j].id-treatments[j+1].id).should.equal(1);
-                            }
-                            //Delete all new treatments added
-                            model.Treatment.destroy({
-                                where: {
-                                    [Op.or]: [{id: treatments[0].id},{id: treatments[1].id}, 
-                                    {id: treatments[2].id}, {id: treatments[3].id}, {id: treatments[4].id}]
-                                }
-                            });
-                            done();
-                        });
-                    }
-                count++;
+                // Get latest created entry in treatment table to fetch the updated data
+                model.Treatment.findOne({
+                    where: {
+                        id: treatments[0].id
+                    },
+                }).then(updatedTreatment => {
+                    updatedTreatment.description.should.not.equal(treatments[0].description);
+                    updatedTreatment.TreatmentTypeId.should.not.equal(treatments[0].TreatmentTypeId);
+                    done();
+                });
             });
-        }
+        });
     });
 });
 
+describe('Test of deleting a treatment', function() {
+    var nbrOfTreatmentsBefore;
+    var treatmentID;
+    beforeEach(function (done) {
+        var data = {description: 'Treatment created', TreatmentTypeId: '2'};
+        //creating a treatment
+        model.Treatment.create(data)
+	    .then(response => {
+            treatmentID = response.id;
+            // Fetch all entries from treatment table
+            model.Treatment.findAll()
+            .then(function(treatments) {
+                nbrOfTreatmentsBefore = treatments.length;
+                done();
+            });
+        });
+    });
+    it('Test of deleting a treatment to check that it has been removed from the database', function (done) {
+        //Send a request to database for deleting the treatment
+        supertest(app)
+        .delete('/treatment/' + treatmentID)
+        .expect(302)
+        .end((err, res) => {
+            should.not.exist(err);
+            res.headers.location.substring(0,6).should.not.equal('/error/');
+            // Fetch all entries from treatment table
+            model.Treatment.findAll()
+            .then(function(treatments) {
+                //Check that the created treatment is gone by first comparing the numbers of entries in the treatment table before and after
+                treatments.length.should.equal(nbrOfTreatmentsBefore-1);
+                //Check that the created treatment is gone by checking if the deleted treatment's id is not in the treatment table
+                for(var i=0; i<treatments.length; i++){
+                    treatments[i].id.should.not.equal(treatmentID);
+                }
+                done();
+            });
+        });
+    });
+});
